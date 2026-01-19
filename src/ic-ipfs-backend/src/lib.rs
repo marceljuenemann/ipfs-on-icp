ac6a6122325416f2335d3a89db992d3664b417ac
Marcel Juenemann <mail@marcel-juenemann.de>
2026-01-19 21:34:27 +0000

    First HTTP response

--- src/ic-ipfs-backend/src/lib.rs
+++ src/ic-ipfs-backend/src/lib.rs
@@ -1,5 +1,9 @@
 mod ipfs;
 
+use std::vec;
+
+use ic_http_certification::{HttpRequest, HttpResponse, StatusCode};
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
@@ -9,6 +13,20 @@ fn greet(name: String) -> String {
 fn store_block(cid: String, data: Vec<u8>) -> Result<(), String> {
     ipfs::parse_cid(&cid);
     Ok(())
+}
+
+#[ic_cdk::query]
+fn http_request(req: HttpRequest) -> HttpResponse {
+    let path = req.get_path().expect("Failed to parse request path");
+
+    ic_cdk::println!("Received HTTP request for path: {}", path);
+
+    let response = HttpResponse::builder()
+        .with_status_code(StatusCode::OK)
+        .with_body("Hello World!".as_bytes())
+        .build();
+
+    response
 }
 
 ic_cdk::export_candid!();

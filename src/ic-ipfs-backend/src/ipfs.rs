7313db0e04b1d8ea43b6b6623cdc2a22e1c198f0
Marcel Juenemann <mail@marcel-juenemann.de>
2026-01-19 19:16:50 +0000

    Add cid library to backend

--- src/ic-ipfs-backend/src/ipfs.rs
+++ src/ic-ipfs-backend/src/ipfs.rs
@@ -1,11 +1,15 @@
+use multihash_codetable::{Code, MultihashDigest};
+use cid::Cid;
+use std::convert::TryFrom;
 
+const RAW: u64 = 0x55;
 
 pub fn parse_cid(cid: &str) -> Result<Vec<u8>, String> {
-    // Dummy implementation for illustration purposes
-    if cid.is_empty() {
-        Err("CID cannot be empty".to_string())
-    } else {
-        Ok(cid.as_bytes().to_vec())
-    }
-}
 
+    let h = Code::Sha2_256.digest(b"beep boop");
+
+    let cid = Cid::new_v1(RAW, h);
+
+    ic_cdk::println!("CID: {}", cid.to_string());
+    Ok(cid.to_bytes())
+}

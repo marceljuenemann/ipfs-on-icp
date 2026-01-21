9d36ebbd4265b05757db890304fb028a36ee3389
Marcel Juenemann <mail@marcel-juenemann.de>
2026-01-21 17:04:01 +0000

    Add custom agents.ts

--- src/ic-ipfs-frontend/src/app/backend.ts
+++ src/ic-ipfs-frontend/src/app/backend.ts
@@ -1,9 +1,6 @@
-import { createActor, ic_ipfs_backend } from 'declarations/ic-ipfs-backend';
-
-import { Injectable, isDevMode } from '@angular/core';
+import { BACKEND } from '../agents';
+import { Injectable } from '@angular/core';
 import { IpfsBlock } from './ipfs';
-import { ActorSubclass } from '@icp-sdk/core/agent';
-import { _SERVICE } from 'declarations/ic-ipfs-backend/ic-ipfs-backend.did';
 
 export type UploadResult = 'success' | 'duplicate' | { error: string };
 
@@ -11,24 +8,10 @@ export type UploadResult = 'success' | 'duplicate' | { error: string };
   providedIn: 'root',
 })
 export class Backend {
-  private actor: ActorSubclass<_SERVICE>;
-
-  constructor() {
-    // TODO: put this into actors.ts. Don't use index.js from the declarations,
-    // as that always throws an error when using with ng serve.
-    if (isDevMode()) {
-      this.actor = createActor(
-        globalThis.process.env['CANISTER_ID_IC_IPFS_BACKEND'],
-        { agentOptions: { host: 'http://localhost:4943' } }
-      );
-    } else {
-      this.actor = ic_ipfs_backend;
-    }
-  }
 
   async uploadBlock(block: IpfsBlock): Promise<UploadResult> {
     try {
-      const result = await this.actor.blockstore_put(block.cid.toString(), block.bytes);
+      const result = await BACKEND.blockstore_put(block.cid.toString(), block.bytes);
       if ('Ok' in result) {
         return result.Ok ? 'success' : 'duplicate';
       } else {

b471b71d61f4c771e08e54a5cd35dabaa095b095
Marcel Juenemann <mail@marcel-juenemann.de>
2026-01-21 18:39:19 +0000

    Implement service worker. Image and video loading working :)

--- src/ic-ipfs-frontend/src/app/app.ts
+++ src/ic-ipfs-frontend/src/app/app.ts
@@ -52,33 +52,4 @@ export class App {
     statusMap.set(cid, result);
     this.uploadStatus.set(statusMap);
   }
-
-  async init() {
-    /*
-    const resp = await fetch('http://uxrrr-q7777-77774-qaaaq-cai.raw.localhost:4943/ipfs/bafkreieq5jui4j25lacwomsqgjeswwl3y5zcdrresptwgmfylxo2depppq');
-    */
-
-    /*
-    const fetch = await createVerifiedFetch({
-      gateways: [
-        // TODO: configure based on canister environment var.
-        'http://uxrrr-q7777-77774-qaaaq-cai.raw.localhost:4943'
-//        'https://trustless-gateway.link'
-      ],
-      routers: [],
-      allowInsecure: true,  // TODO: dev only
-      allowLocal: true  // TODO: dev only
-    })
-
-    const resp = await fetch('ipfs://bafkreid7qoywk77r7rj3slobqfekdvs57qwuwh5d2z3sqsw52iabe3mqne', {
-      onProgress: (event) => {
-        console.log(event);
-      }
-    });
-
-
-    console.log(resp);
-    console.log(await resp.text());
-    */
-  }
 }
--- src/ic-ipfs-frontend/src/sw/sw.ts
+++ src/ic-ipfs-frontend/src/sw/sw.ts
@@ -1,4 +1,34 @@
+/// <reference lib="webworker" />
 
+import { createVerifiedFetch } from '@helia/verified-fetch';
+import { clientsClaim } from 'workbox-core';
+import { registerRoute } from 'workbox-routing';
 
+declare const self: ServiceWorkerGlobalScope;
 
-console.log("Hello World! This is the service worker!");
+// Active as soon as possible.
+clientsClaim();
+self.skipWaiting();
+
+const verifiedFetch = createVerifiedFetch({
+  gateways: [
+    // TODO: configure based on canister environment var.
+    'http://uxrrr-q7777-77774-qaaaq-cai.raw.localhost:4943'
+    // TODO: Use public gateway as fallback
+    // 'https://trustless-gateway.link'
+  ],
+  routers: [],  // No p2p fetching.
+  allowInsecure: true,  // TODO: dev only
+  allowLocal: true  // TODO: dev only
+})
+
+// IPFS handling.
+// TODO: Implement caching strategies for IPFS content.
+registerRoute(
+  // TODO: Do not allow for direct navigations, otherwise every file could read cookies.
+  ({ url }) => url.pathname.startsWith('/ipfs/') || url.pathname.startsWith('/ipns/'),
+  async ({ url }) => {
+    const fetch = await verifiedFetch;
+    return await fetch(url.pathname);
+  }
+);
